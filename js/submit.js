ae744b62d3d9056e428c943192545a7a17919c56
ShakawatFahim <mdshakawathussain52@gmail.com>
2022-02-16 03:18:45 +0600

    '2nd'

--- js/submit.js
+++ js/submit.js
@@ -3,12 +3,17 @@ document.getElementById('submit-btn').addEventListener('click', function () {
     const email = emailField.value;
     const passwordField = document.getElementById('password');
     const password = passwordField.value;
-
+    const right = document.getElementById('right');
+    const wrong = document.getElementById('wrong');
     if (email == 'fahimema@gmail.com' && password == 'lovealsa') {
-        window.location.href = 'banking.html'
+        window.location.href = 'banking.html';
+        right.style.display = 'block';
+        wrong.style.display = 'none';
     }
     else {
         emailField.value = '';
         passwordField.value = '';
+        right.style.display = 'none';
+        wrong.style.display = 'block';
     }
 })
